79927f5233029cb9a86dd96a816126b33f84f0c1
terao nana <amnosonoarashi@gmail.com>
2016-03-13 01:39:04 +0900

    Change of the indication of the date

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 20160208141132) do
 
   create_table "groups", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
+    t.datetime.to_s(:default) "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
